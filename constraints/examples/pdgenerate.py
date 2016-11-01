fc0dcc41ce8ae0a900a1aee857da4c8ee004dfaf
Nicholas J. Radcliffe <njr@StochasticSolutions.com>
2016-11-01 18:00:50 +0000

    Made pdgenerate.py example more useful.

--- constraints/examples/pdgenerate.py
+++ constraints/examples/pdgenerate.py
@@ -3,11 +3,15 @@
 from __future__ import print_function
 import pandas as pd
 
-from tdda.constraints.pdconstraints import discover_constraints
+from tdda.constraints.pdconstraints import discover_constraints, verify_df
 
 df = pd.read_csv('../testdata/elements92.csv')
 constraints = discover_constraints(df)
 with open('elements92.tdda', 'w') as f:
     f.write(constraints.to_json())
 print('Written elements92.tdda')
+
+print(verify_df(df, 'elements92.tdda'))
+
+
 
