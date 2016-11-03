8933e0b1aa2a5310a7dcaea3254f0b7b475977d8
Nicholas J. Radcliffe <njr@StochasticSolutions.com>
2016-11-03 14:58:05 +0000

    Added a little more whitespace to some example code.

--- constraints/examples/simple_verify_fail.py
+++ constraints/examples/simple_verify_fail.py
@@ -6,6 +6,7 @@ df = pd.DataFrame({'a': [0, 1, 2, 10, pd.np.NaN],
 v = verify_df(df, 'example_constraints.tdda')
 
 print('Passes: %d' % v.passes)
-print('Failures: %d\n' % v.failures)
+print('Failures: %d\n\n\n' % v.failures)
 print(str(v))
+print('\n\n')
 print(v.to_frame())
--- constraints/examples/simple_verify_pass.py
+++ constraints/examples/simple_verify_pass.py
@@ -5,6 +5,7 @@ df = pd.DataFrame({'a': [2, 4], 'b': ['one', pd.np.NaN]})
 v = verify_df(df, 'example_constraints.tdda')
 
 print('Passes: %d' % v.passes)
-print('Failures: %d\n' % v.failures)
+print('Failures: %d\n\n\n' % v.failures)
 print(str(v))
+print('\n\n')
 print(v.to_frame())
