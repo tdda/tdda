1c4fdcb45b9d593480d48309958cf7e85e4da2e9
Simon Brown <simon@StochasticSolutions.com>
2017-01-10 15:17:32 +0000

    Added missing file testregeneration.py.

---
+++ referencetest/tests/testregeneration.py
@@ -0,0 +1,61 @@
+# -*- coding: utf-8 -*-
+
+#
+# Unit tests for reference data regeneration
+#
+
+from __future__ import absolute_import
+from __future__ import unicode_literals
+from __future__ import division
+
+import os
+import tempfile
+import unittest
+
+from tdda.referencetest.referencetest import ReferenceTest
+
+
+class TestRegenerate(ReferenceTest):
+    @classmethod
+    def setUpClass(cls):
+        cls.tmpdir = tempfile.gettempdir()
+
+    def setUp(self):
+        ReferenceTest.set_default_data_location(self.tmpdir)
+
+    def tearDown(self):
+        ReferenceTest.set_regeneration(False)
+        
+    def test_regenerate_all(self):
+        ReferenceTest.set_regeneration()
+        ref = ReferenceTest(assert_fn=self.assertTrue)
+        refname = 'regenerate_all.txt'
+        reffile = os.path.join(self.tmpdir, refname)
+        ref.assertStringCorrect('Start\nMiddle\nEnd\n', refname)
+        with open(reffile) as f:
+            self.assertEqual(f.read(), 'Start\nMiddle\nEnd\n')
+        ref.assertStringCorrect('Completely different content', refname)
+        with open(reffile) as f:
+            self.assertEqual(f.read(), 'Completely different content')
+
+    def test_regenerate_kinds(self):
+        ReferenceTest.set_regeneration('csv')
+        ref = ReferenceTest(assert_fn=self.assertTrue)
+        txtname = 'regenerate.txt'
+        csvname = 'regenerate.csv'
+        txtfile = os.path.join(self.tmpdir, txtname)
+        csvfile = os.path.join(self.tmpdir, csvname)
+        with self.assertRaises(Exception):
+            self.assertStringCorrect('Start\nMiddle\nEnd\n', txtname,
+                                     kind='txt')
+        self.assertStringCorrect('Start\nMiddle\nEnd\n', csvname, kind='csv')
+        with open(csvfile) as f:
+            self.assertEqual(f.read(), 'Start\nMiddle\nEnd\n')
+        ref.assertStringCorrect('Completely different content', csvname,
+                                kind='csv')
+        with open(csvfile) as f:
+            self.assertEqual(f.read(), 'Completely different content')
+
+
+if __name__ == '__main__':
+    unittest.main()
