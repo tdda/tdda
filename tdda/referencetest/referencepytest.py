e621c7dfdb04d4aad62396821d3bafdd1eae54dc
Simon Brown <simon@StochasticSolutions.com>
2017-04-05 11:15:57 +0100

    Referencetest no longer imports unicode_literals from future.
    (This was causing problems when running test code that wasn't itself
    using unicoe string literals, resulting in a mixture of str and unicode
    strings in lists of error messages.)

--- tdda/referencetest/referencepytest.py
+++ tdda/referencetest/referencepytest.py
@@ -74,7 +74,6 @@ with the ``pytest`` framework.
 from __future__ import absolute_import
 from __future__ import print_function
 from __future__ import division
-from __future__ import unicode_literals
 
 import sys
 
