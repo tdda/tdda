c8b55e94f6e96d3ad303fdd179c6861bde7ebc2a
Simon Brown <simon@StochasticSolutions.com>
2017-08-19 10:49:56 +0100

    tdda documentation conf.py now includes explicit sys.path (so that RTD can import version.py from the right place when building docs)

--- doc/source/conf.py
+++ doc/source/conf.py
@@ -15,6 +15,7 @@
 import sys
 import os
 
+sys.path.append(os.path.join(os.path.abspath(__file__), '../../..'))
 from tdda import __version__ as version
 
 # If extensions (or modules to document with autodoc) are in another directory,
