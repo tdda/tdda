7e103b13485ded0186817e6cd55c16bf8f50bbbb
Simon Brown <simon@StochasticSolutions.com>
2018-09-18 16:17:45 +0100

    Update to version tdda-1.0.17

--- tdda/CHANGES.py
+++ tdda/CHANGES.py
@@ -74,6 +74,6 @@ also not matched without re.DOTALL, including a non-breaking space (0x80).
 18.09.2018 1.0.16
 referencetest class now exports TaggedTestLoader for convenience.
 
-###
+18.09.2018 1.0.17
 TestLoader now takes an optional 'printer' parameter to control how -0 works.
 """
