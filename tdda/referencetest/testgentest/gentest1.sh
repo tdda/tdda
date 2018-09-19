1e81708fd7e2a386a9c6541b06cb37bb5a25db84
Nicholas J. Radcliffe <njr@StochasticSolutions.com>
2018-09-19 11:51:12 +0100

    Renamed gentesttests as testgentest

---
+++ tdda/referencetest/testgentest/gentest1.sh
@@ -0,0 +1,14 @@
+#
+# Generates 2files test successfully, specifying non-zero exit code is allowed
+#
+# Currently requires STDOUT test to get exclusions added for
+# command timings, because it is not generated automatically.
+#
+
+CMD='tdda gentest -r "python 2files.py" 2files . stdout stderr nonzeroexit'
+tdda gentest "$CMD" meta_2files.py \
+                    ref/2files \
+                    STDOUT \
+                    STDERR
+
+
