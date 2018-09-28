71aa19fd71b7b2648bd647b967d53dc36650509a
Nicholas J. Radcliffe <njr@StochasticSolutions.com>
2018-09-28 17:46:07 +0100

    Added second manual gentest test.

--- tdda/referencetest/testgentest/test_gentest.py
+++ tdda/referencetest/testgentest/test_gentest.py
@@ -12,26 +12,37 @@ import datetime
 import os
 import sys
 
-from tdda.referencetest import ReferenceTestCase
+from tdda.referencetest import ReferenceTestCase, tag
 from tdda.referencetest.gentest import TestGenerator
 
 D1 = '/home/auser/python/tdda/tdda/referencetest/gentest'
 HOST = 'ahost.local'
+ALTHOST = 'ahost'
 HOME = '/home/auser'
 LOCALHOST = '127.0.0.1'   # not used
 USER = 'auser'
 
 class TestGenTest(ReferenceTestCase):
-    def test1_date_exclusions(self):
+    def test1_exclusions(self):
         name = 'test_gentest1'
+        # Create a test generator that doesn't actually run the tests
+        # (bcause of iterations=0)
         t = TestGenerator(cwd=D1, command='', script=name,
                           reference_files=['STDOUT'],
                           check_stdout=True,
                           check_stderr=False, require_zero_exit_code=True,
                           iterations=0, verbose=False)
 
+        # Point the reference directory to the right place, which
+        # the ref directory next to this file.
         t.refdir = os.path.join(os.path.dirname(os.path.abspath(__file__)),
                                 'ref', name)
+
+        # Fix everything else to be as if the modified Miro script output
+        # had been on the host HOST, with the cwd as D1
+        # and had run between 16:30:00 and 16:30:02 on 1st July 2020,
+        # and iterations had actually been 2.
+
         t.host = HOST
         t.user = USER
         t.ip = LOCALHOST
@@ -55,6 +66,52 @@ r'ahost\.local',
         }
         self.assertEqual(set(t.exclusions['STDOUT'][0]), expected)
         self.assertEqual(set(t.exclusions['STDOUT'][1]), {'ODD'})
+
+    @tag
+    def test2_simple_date_exclusions(self):
+        name = 'test_gentest2'
+
+        # Create a test generator that doesn't actually run the tests
+        # (bcause of iterations=0)
+        t = TestGenerator(cwd=D1, command='', script=name,
+                          reference_files=['STDOUT'],
+                          check_stdout=True,
+                          check_stderr=False, require_zero_exit_code=True,
+                          iterations=0, verbose=False)
+
+        # Point the reference directory to the right place, which
+        # the ref directory next to this file.
+        t.refdir = os.path.join(os.path.dirname(os.path.abspath(__file__)),
+                                'ref', name)
+
+        # Fix everything else to be as if the modified Miro script output
+        # had been on the host HOST, with the cwd as D1
+        # and had run between 16:30:00 and 16:30:02 on 1st July 2020,
+        # and iterations had actually been 2.
+
+        t.host = ALTHOST
+        t.user = USER
+        t.ip = LOCALHOST
+        t.homedir = HOME
+        t.min_time = datetime.datetime(2020,7,1,16,30,00)
+        t.max_time = datetime.datetime(2020,7,1,16,30,02)
+        t.reference_files[1] = os.path.join(t.refdir, 'STDOUT')
+        t.reference_files[2] = os.path.join(t.refdir, '2', 'STDOUT')
+        t.iterations = 2
+
+        t.generate_exclusions()
+
+        expected = {
+r'^Plausible date 2020\-07\-01 12\:00\:\d{2} and the hostname ahost  \# diff$',
+r'2020-07-01 12:00:13',
+r'2020-07-01 13:00:13',
+r'auser',
+# 2018-09-02            out of range date
+# 2018-09-02 03:00:00   out of range datetime
+# ENDS                  common
+        }
+        self.assertEqual(set(t.exclusions['STDOUT'][0]), expected)
+        self.assertEqual(set(t.exclusions['STDOUT'][1]), set())
 
 
 if __name__ == '__main__':
