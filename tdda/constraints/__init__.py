9026ec49fe902695ef10daffa7f7a70ea0d301b0
Simon Brown <simon@StochasticSolutions.com>
2019-06-25 18:24:52 +0100

    Fixed a test that had been affected by changes elsewhere

--- tdda/constraints/__init__.py
+++ tdda/constraints/__init__.py
@@ -1,4 +1,15 @@
-from tdda.constraints.pd.constraints import discover_df, verify_df, detect_df
-from tdda.constraints.db.constraints import (discover_db_table,
-                                             verify_db_table,
-                                             detect_db_table)
+
+try:
+    from tdda.constraints.pd.constraints import (discover_df,
+                                                 verify_df,
+                                                 detect_df)
+except ImportError:
+    pass
+
+try:
+    from tdda.constraints.db.constraints import (discover_db_table,
+                                                 verify_db_table,
+                                                 detect_db_table)
+except ImportError:
+    pass
+
