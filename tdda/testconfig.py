221bf7e5faf612feab30680b065a81b85f0ac634
Nick Radcliffe <njr@StochasticSolutions.com>
2022-01-07 09:58:48 +0000

    Upgrade config tests.

--- tdda/testconfig.py
+++ tdda/testconfig.py
@@ -6,16 +6,24 @@ import unittest
 class TestSystemConfig(unittest.TestCase):
     def test_01_tdda_path(self):
         print('\ntype tdda')
-        os.system('type tdda')
+        with os.popen('type tdda') as f:
+            path = f.read()
+        print(path)
         print('which tdda')
-        os.system('which tdda')
+        with os.popen('which tdda') as f:
+            path = f.read()
+        print(path)
 
     def test_02_path(self):
-        print('$PATH')
-        os.system('echo $PATH')
+        path = os.environ.get('PATH')
+        print('PATH=%s' % path)
+        print('COMPONENTS:')
+        for p in path.split(':'):
+            print(p)
+        print()
 
     def test_03_pythonpath(self):
-        print('PYTHON PATH')
+        print('PYTHON PATH:')
         for p in sys.path:
             print(p)
 
