c6fb49c57004d5ff0279f1f112e8ddc2ed3dccda
Nick Radcliffe <njr@StochasticSolutions.com>
2022-01-23 17:46:34 +0000

    Fix error in conf.py.

--- doc/source/conf.py
+++ doc/source/conf.py
@@ -123,7 +123,7 @@ html_theme = 'default'
 
 # The name of an image file (relative to this directory) to place at the top
 # of the sidebar.
-cus html_logo = '_static/tdda-logo400x400-TDDA.png'
+html_logo = '_static/tdda-logo400x400-TDDA.png'
 
 # The name of an image file (within the static path) to use as favicon of the
 # docs.  This file should be a Windows icon file (.ico) being 16x16 or 32x32
