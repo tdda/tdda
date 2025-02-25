83f4148e83158715d6c09de79e51356006a2bca8
Nick Radcliffe <njr@stochasticsolutions.com>
2025-02-25 08:10:00 +0000

    Better conditional imports for pytest

--- tdda/constraints/testconstraints.py
+++ tdda/constraints/testconstraints.py
@@ -6,22 +6,23 @@ from tdda.constraints.testbase import *
 from tdda.referencetest import ReferenceTestCase
 
 try:
-    from tdda.constraints.db.testdbconstraints import (
-        TestSQLiteDBHandlers,
-        TestPostgresDBHandlers,
-        TestMySQLDBHandlers,
-
-        TestSQLiteConstraintVerifiers,
-        TestPostgresConstraintVerifiers,
-        TestMySQLConstraintVerifiers,
-    )
-
+    from tdda.constraints.pd.testpdconstraints import *
 except ImportError:
     print('Skipping Pandas tests', file=sys.stderr)
 
 try:
-    # from tdda.constraints.db.testdbconstraints import *
-    pass
+    from tdda.constraints.db.testdbconstraints import (
+        TestSQLiteDBHandlers,
+        TestPostgresDBHandlers,
+        TestMySQLDBHandlers,
+        TestSQLiteDatabaseConnectionFile,
+        TestMongoDBHandlers,
+        TestSQLiteDBConstraintVerifiers,
+        TestSQLiteDBConstraintDiscoverers,
+        TestPostgresDBConstraintDiscoverers,
+        TestMySQLDBConstraintDiscoverers,
+        TestMongoDBConstraintDiscoverers,
+    )
     # The individual imports of the database driver libraries
     # are now all protected with try...except blocks,
     # so this try...except is probably now unnecessary.
