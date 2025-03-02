e7262b843382c50e45fa529d84d9ce403ded670b
Nick Radcliffe <njr@StochasticSolutions.com>
2025-03-02 19:47:10 +0000

    .

--- tdda/utils.py
+++ tdda/utils.py
@@ -1,5 +1,19 @@
+import os
+
 def nvl(v, w):
     """
     This function is used as syntactic sugar for replacing null values.
     """
     return w if v is None else v
+
+
+def handle_tilde(path):
+    """
+    Handle paths starting tilde.
+
+    Does nothing unless path is a string and starts with '~'
+    """
+    if type(path) is str and path.startswith('~'):
+        return os.path.expanduser(path)
+    else:
+        return path
