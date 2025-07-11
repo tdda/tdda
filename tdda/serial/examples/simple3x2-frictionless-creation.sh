5eafd63ccce0c0ede60e7e296c9636536084a27c
Nick Radcliffe <njr@stochasticsolutions.com>
2025-07-11 08:37:27 +0100

    Removed --field-missing-values "" from fless cmds
    It seems frictionless CLI won't let you explicitly specify the
    empty string as a null marker.

--- tdda/serial/examples/simple3x2-frictionless-creation.sh
+++ tdda/serial/examples/simple3x2-frictionless-creation.sh
@@ -1,13 +1,13 @@
 #!/bin/sh
 
-frictionless describe simple3x2.csv --json --type resource --field-missing-values "" --dialect '{"delimiter": ",", "quoteChar": "\"", "escapeChar": "\\", "header": true, "header_rows": [0], "doubleQuote": false}' > simple3x2.resource.json
+frictionless describe simple3x2.csv --json --type resource --dialect '{"delimiter": ",", "quoteChar": "\"", "escapeChar": "\\", "header": true, "header_rows": [0], "doubleQuote": false}' > simple3x2.resource.json
 
-frictionless describe simple3x2.csv --yaml --type resource --field-missing-values "" --dialect '{"delimiter": ",", "quoteChar": "\"", "escapeChar": "\\", "header": true, "header_rows": [0], "doubleQuote": false}' > simple3x2.resource.yaml
+frictionless describe simple3x2.csv --yaml --type resource --dialect '{"delimiter": ",", "quoteChar": "\"", "escapeChar": "\\", "header": true, "header_rows": [0], "doubleQuote": false}' > simple3x2.resource.yaml
 
 
-frictionless describe simple3x2.csv --json --type package --field-missing-values "" --dialect '{"delimiter": ",", "quoteChar": "\"", "escapeChar": "\\", "header": true, "header_rows": [0], "doubleQuote": false}' > simple3x2.package.json
+frictionless describe simple3x2.csv --json --type package --dialect '{"delimiter": ",", "quoteChar": "\"", "escapeChar": "\\", "header": true, "header_rows": [0], "doubleQuote": false}' > simple3x2.package.json
 
-frictionless describe simple3x2.csv --yaml --type package --field-missing-values "" --dialect '{"delimiter": ",", "quoteChar": "\"", "escapeChar": "\\", "header": true, "header_rows": [0], "doubleQuote": false}' > simple3x2.package.yaml
+frictionless describe simple3x2.csv --yaml --type package --dialect '{"delimiter": ",", "quoteChar": "\"", "escapeChar": "\\", "header": true, "header_rows": [0], "doubleQuote": false}' > simple3x2.package.yaml
 
 
 
