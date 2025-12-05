4b8f211d836d600d9dfdeba5139385840abcf61f
Nick Radcliffe <njr@StochasticSolutions.com>
2025-12-05 14:50:10 +0000

    Another setup tweak

--- setup.py
+++ setup.py
@@ -60,7 +60,7 @@ setup(
                                     ['init']),
         'tdda.rexpy': data(['tdda', 'rexpy'], ['examples']),
         'tdda.gentest': data(['tdda', 'gentest'], ['examples']),
-        'tdda': ['README.md', 'LICENSE.txt', 'templates'],
+        'tdda': ['README.md', 'LICENSE.txt', 'templates/tdda/css'],
         'tdda.serial': data(['tdda', 'serial'], ['testdata']),
     },
     entry_points = {
