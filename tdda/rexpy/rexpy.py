137e3db1e7b7cf71ba1ce02c85b9c5eba8ed9cd1
Nick Radcliffe <njr@StochasticSolutions.com>
2026-07-03 08:21:45 +0100

    Add doc for -1/--single in rexpy. Fix doc test.
    Rexpy now supports a flag -1 or --single to ask for all
    regular expressions found to be combined into a single alternation
    (in cases where there is more than one).
    
    This is now added to the help (rexpy --help) and (Sphinx)
    documentation and also to the man page.
    
    A doc test (i.e. claim in the documentation) was outdated,
    missing a utf-8 line from an inferred serial file.
    This is now corrected.

--- tdda/rexpy/rexpy.py
+++ tdda/rexpy/rexpy.py
@@ -153,6 +153,12 @@ Optional FLAGS may be used to modify Rexpy's behaviour:
                     Mostly useful for matching identifiers.
                     Also --hyphen or --dash.
 
+  -1, --single      Combine multiple regular expressions found into a
+                    single regular expression, using alternation.
+                        e.g.     ^[A-Z]{2}$
+                                 ^[0-9]{3}$
+                        becomes  ^([A-Z]{2}|[0-9]{3})$
+
   -v, --version     Print the version number.
 
   -V, --verbose     Set verbosity level to 1
